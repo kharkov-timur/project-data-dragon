63f7d0b6d3639d65f431e4156bbe1205edafe155
Kharkov Timur <timur.kharkov@gmail.com>
2023-12-19 10:37:39 +0200

    Added to README file project description

--- main.py
+++ main.py
@@ -1,1 +1,85 @@
-print("Hello main")
+from commands import (
+    add_contact,
+    change_contact,
+    find_phone,
+    show_all,
+    add_birthday,
+    find_birthday,
+    birthdays,
+    save_contacts,
+    load_contacts,
+)
+from address_book import AddressBook
+
+
+MENU = """
+MENU:
+    # menu - show menu
+    # add-contact - [name] [phone] [birthday(optional)] - add new contact
+    # change-contact - [name] [phone] - change contact number
+    # find-phone - [name] - show contact phone
+    # all-contacts - show all contacts
+    # add-birthday - [name] [birthday] - add birthday to contact
+    # find-birthday - [name] - show birthday of contact
+    # birthdays - show upcoming birthdays
+    # save-contacts - save all contacts
+    # load-contacts - load all contacts
+    # exit/close - exit from program
+"""
+
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, args
+
+
+def main():
+    book = AddressBook()
+    print("\nWelcome to the PERSONAL CONTACT HELPER!")
+    print(MENU)
+
+    while True:
+        user_input = input("Enter a command: ")
+        command, args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "menu":
+            print(MENU)
+
+        elif command == "add-contact":
+            print(add_contact(args, book))
+
+        elif command == "change-contact":
+            print(change_contact(args, book))
+
+        elif command == "find-phone":
+            print(find_phone(args, book))
+
+        elif command == "all-contacts":
+            print(show_all(book))
+
+        elif command == "add-birthday":
+            print(add_birthday(args, book))
+
+        elif command == "find-birthday":
+            print(find_birthday(args, book))
+
+        elif command == "birthdays":
+            print(birthdays(book))
+
+        elif command == "save-contacts":
+            print(save_contacts(book))
+
+        elif command == "load-contacts":
+            print(load_contacts(book))
+
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()
